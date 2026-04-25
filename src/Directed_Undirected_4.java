80d93489c7f8c29ea25dc0662910c72a638b0e09
lauren-m63 <lauren.m63@yahoo.com>
2026-04-25 16:29:35 -0500

    4

--- src/Directed_Undirected_4.java
+++ src/Directed_Undirected_4.java
@@ -19,12 +19,22 @@ notes:
 int[m][n] - so m is the rows and n is the columsn matrix [row][column]
  */
 
-public void isConnected(int [][] inputMatrix){
-    int n = inputMatrix.length; // i can just get length of matrix with this cool since its just a double array you can use the array functions
-    boolean[] visited = new boolean[n]; // array of booleans that let me know if its visited not double arrya though ?
+public boolean isConnected(int [][] adjMatrix){ // taking an adjanecy matrix not an actual matrix just the numbers saying true/false if they are connected or not with edge
+    int n = adjMatrix.length; // i can just get length of matrix with this cool since its just a double array you can use the array functions
+        // this is the number of rows for length though so this is the number of vertices in the graph
+        // since its an adjceny graph the rows and columsn are the same thouhgh for this but usually its by row anyway?
+    boolean[] visited = new boolean[n]; // array of booleans that let me know if its visited not double arrya though
+        //each index represents the node since its likegoing by the length which is teh number of vertexes
+        // so this is creating a list of boolean with length n which is how many nodes there are- so for each row its like have we visited this one
 
-
+    DFS(0, adjMatrix, visited);
     // since its recursive search method im gonna call it from here yas
+    for (int i = 0; i<n; i++ ) {
+        if (!visited[i]) { // so i do dfs and then now im like were they all visited and if not then its not connected
+            return false;
+        }
+        return true;
+    }
 
 } // END ISCONENCTED METHOD
 
@@ -32,6 +42,19 @@ public void isConnected(int [][] inputMatrix){
 
 
     public static void DFS(int node, int [][] inputMatrix, boolean[] visited){
+        // dfs is the depth one so goes down all the chain and visits adjacent enighbors then goes all the way up to the top level unvisited one
+
+        visited[node] = true; // marking a node as visited why the first line though?
+        int n = inputMatrix.length; // num of nodes so i can loop it
+
+
+        for ( int i = 0; i < n; i++ ){ // for all the nodes if they have not been visited and there is a connection to another then do this again but with my new node
+                // so when i do it again im marking the one im doing as visited and getting the rows for my column and looking at all the neighbors of my curren cool
+            if (!visited[i] && (inputMatrix[node][i] == 1 || inputMatrix[i][node] == 1) ){
+                DFS(i,inputMatrix,visited);
+            }
+        }
+
 
     } // END DFS METHOD
 
