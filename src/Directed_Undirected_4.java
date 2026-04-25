a104e4cb3c9337448cfdcb066269aa4c9062a342
lauren-m63 <lauren.m63@yahoo.com>
2026-04-25 15:57:13 -0500

    starting 4

--- src/Directed_Undirected_4.java
+++ src/Directed_Undirected_4.java
@@ -1,5 +1,21 @@
 public class Directed_Undirected_4 {
 
+/*
+write program thta takes adjacency representation of a graph with n nodes as input
+and determines whether that matrix is the representation of a weakly connected graph
+ */
 
 
-}
+/*
+so basically im trying to see if every node is reachable from every node so if they are all connected
+but ignoring edge directions so weakly connected
+so i can use my dfs or bfs since cost and direction doesnt matter
+
+to see if connected i just trakc the visited nodes and if i visit all of them then its weakly connected
+like if there isnt a disconnected random in the graph then its chill
+
+ */
+
+
+
+} // LAST BRACKET END CLASS
