9ea9913024a4afc099c3b10e4875e6511385377f
lauren-m63 <lauren.m63@yahoo.com>
2026-04-25 16:02:20 -0500

    4

--- src/Directed_Undirected_4.java
+++ src/Directed_Undirected_4.java
@@ -14,7 +14,28 @@ so i can use my dfs or bfs since cost and direction doesnt matter
 to see if connected i just trakc the visited nodes and if i visit all of them then its weakly connected
 like if there isnt a disconnected random in the graph then its chill
 
+
+notes:
+int[m][n] - so m is the rows and n is the columsn matrix [row][column]
  */
+
+public void isConnected(int [][] inputMatrix){
+    int n = inputMatrix.length; // i can just get length of matrix with this cool since its just a double array you can use the array functions
+    boolean[] visited = new boolean[n]; // array of booleans that let me know if its visited not double arrya though ?
+
+
+    // since its recursive search method im gonna call it from here yas
+
+} // END ISCONENCTED METHOD
+
+
+
+
+    public static void DFS(int node, int [][] inputMatrix, boolean[] visited){
+
+    } // END DFS METHOD
+
+
 
 
 
